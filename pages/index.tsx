11c883f829a5c8993c89654a0b5a77458781cae0
Emre Ünsal <unsalemre111@gmail.com>
2022-11-26 18:26:14 +0300

    add basic structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import React from 'react';
 
 export default function Home() {
   return (
-    <div className='#app'>
+    <div className="#app">
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
     </div>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
+/* eslint-disable global-require */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],  theme: {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+};
